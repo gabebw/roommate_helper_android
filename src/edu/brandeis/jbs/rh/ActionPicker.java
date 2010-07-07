948e0ab5912b4481c3f9c01f5f654a4410992874
Gabe Berke-Williams <gbw@brandeis.edu>
2010-07-07 16:33:20 -0400

    Initial commit: 2 nonfunctioning UI screens

---
+++ src/edu/brandeis/jbs/rh/ActionPicker.java
@@ -0,0 +1,17 @@
+package edu.brandeis.jbs.rh;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class ActionPicker extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actionpicker);
+    }
+}
---
+++ src/edu/brandeis/jbs/rh/RoommateHelper.java
@@ -0,0 +1,25 @@
+package edu.brandeis.jbs.rh;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+//import android.widget.Button;
+
+public class RoommateHelper extends Activity implements OnClickListener {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        View button = findViewById(R.id.login_button);
+        button.setOnClickListener(this);
+    }
+    
+    public void onClick(View view) {
+    	Intent i = new Intent(RoommateHelper.this, ActionPicker.class);
+    	startActivity(i);
+    }
+}
