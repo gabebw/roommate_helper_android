095f04eedb0d96d5482a580e5e01ce20438acf31
Gabe Berke-Williams <gbw@brandeis.edu>
2010-07-14 00:37:08 -0400

    Add beginnings of Whiteboards (plural) activity

---
+++ src/edu/brandeis/jbs/rh/Whiteboards.java
@@ -0,0 +1,13 @@
+package edu.brandeis.jbs.rh;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Whiteboards extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.whiteboards);
+	}
+}
